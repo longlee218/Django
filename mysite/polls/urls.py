e0aec9ba2589cffd37a74a2e78e66fff3334d01f
longlee218 <54740478+longlee218@users.noreply.github.com>
2019-10-31 22:49:19 +0700

    Demo app Django

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.index, name='index'),
+    path('demo/', views.index, name='index'),
+    path('love/',views.demo, name='love'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,9 @@
 from django.http import HttpResponse
+
+
 def index(request):
     return HttpResponse('Hello world . Long dep trai')
+
+
+def demo(request):
+    return HttpResponse('<h1>Anh yêu em <a href= ''https://www.facebook.com/heoheo.linhlinh?epa=SEARCH_BOX''>Linh Vũ</a></h1>')
