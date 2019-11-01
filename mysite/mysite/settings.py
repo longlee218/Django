76880390fa795f2ae5debafd85a06b642079ba46
longlee218 <54740478+longlee218@users.noreply.github.com>
2019-11-01 21:58:06 +0700

    Database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'do_an',
+        'USER': 'root',
+        'PASSWORD': 'vanmieuNTT218',
+        'HOST': '::1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Demo(models.Model):
+    name = models.CharField(max_length=35)
+    id = models.IntegerField(primary_key=True)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=35)
+    birthday = models.DateTimeField(null=True)
